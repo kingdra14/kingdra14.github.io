e039c920098fdae1fb91004e4d186cca65349d90
Abdoulie Drammeh <700174037@fvtc.edu>
2025-09-12 15:34:19 -0500

    Added welcome div

--- site.js
+++ site.js
@@ -1,5 +1,21 @@
-console.log('hi')
-const welcome = document.querySelector('h1')
+
+const h1 = document.querySelector('h1')
 setTimeout(() => {
-welcome.textContent = 'Welcome to my page ;)'
+h1.textContent = 'Welcome to my page ;)'
 },5000)
+
+const welcome = document.querySelector('#welcome h3')
+const hours = new Date().getHours() // get the current hour
+
+const isMorning = hours >= 4 && hours < 12 // is it morning?
+const isAfternoon = hours >= 12 && hours < 17 // is it afternoon?
+const isEvening = hours >= 17 || hours < 4 // is it evening?
+
+if (isMorning)
+    welcome.textContent ="Good Morning"
+else if (isAfternoon) {
+    welcome.textContent ="Good Afternoon"
+    
+} else {
+    welcome.textContent ="Good Evening"
+}
