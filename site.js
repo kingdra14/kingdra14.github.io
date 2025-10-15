c71e6f84bf23eecb71d7df0af80c49937c332afd
Abdoulie Drammeh <700174037@fvtc.edu>
2025-10-14 21:02:04 -0500

    Added Carousel

--- site.js
+++ site.js
@@ -22,3 +22,43 @@ else if (isAfternoon) {
 
 
 localStorage.setItem('It\'s a secret to everybody', 'Whats up doc?')
+
+const urls = [
+    'https://images.pexels.com/photos/1454360/pexels-photo-1454360.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/933964/pexels-photo-933964.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/267885/pexels-photo-267885.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/1251861/pexels-photo-1251861.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/1370296/pexels-photo-1370296.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+].map(url => { (new Image()).src = url; return url })
+
+const images = document.querySelectorAll('#carousel img')
+
+let currentImage = 0
+const showImages = () => {
+    const offset = currentImage % urls.length
+    images.forEach((image, index) => {
+        const imageIndex = (index + offset + urls.length) % urls.length
+        image.src = urls[imageIndex]
+    })
+}
+
+showImages()
+const btnNext = document.querySelector('#next')
+btnNext.addEventListener('click',() =>
+{
+    currentImage ++
+    showImages()
+})
+
+const btnPrev = document.querySelector('#prev')
+btnPrev.addEventListener('click',() =>
+{
+    currentImage --
+    showImages()
+})
+
+
+setInterval(() => {
+    currentImage ++
+    showImages()
+}, 5000)
